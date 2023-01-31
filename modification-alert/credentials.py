bd5d85357e769799ba6aa7000492029a348c9adb
zekaizhang <zz2919@columbia.edu>
2023-01-30 23:29:24 -0500

    setup for objective #1

---
+++ modification-alert/credentials.py
@@ -0,0 +1,7 @@
+aura_url  = "neo4j+s://5008fc3b.databases.neo4j.io"
+aura_user = "neo4j"
+aura_pw   = "tG1qoEpchdLRh4k5KlCDazCHkgdFHSHATwBtaXVMrdM"
+
+remote_url =  "neo4j://localhost:7687"
+remote_user = "kaiser_zzk"
+remote_pw =   "123465789"
